94c0a91cb3c3b4e860b27c8c7d8109ab56ed2cc5
Naransula <121567218+Naransula@users.noreply.github.com>
2023-03-04 10:16:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
-import asyncio
 import hikari
 import lightbulb
-import random
 import config
-
 
 bot = lightbulb.BotApp(
     config.token,
-    default_enabled_guilds=(config.enabled_guilds), intents=hikari.Intents.ALL_UNPRIVILEGED
+    default_enabled_guilds=(config.enabled_guilds), 
+	intents=hikari.Intents.ALL_UNPRIVILEGED
 )
 
 bot.load_extensions_from("./komutlar")
@@ -15,15 +13,5 @@ bot.load_extensions_from("./komutlar")
 @bot.listen(hikari.StartedEvent)
 async def on_started(event):
     print("Ördek BOT by Naransula#0001")
-
-@lightbulb.Check
-def rp_mod(context: lightbulb.Context) -> bool:
-    return context.author.id == config.rp_mods
-
-@bot.command
-@lightbulb.command("id", "Kullanıcı ID'nizi öğrenin.")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def id(ctx):
-    await ctx.respond(hikari.Embed(title=f"Kullanıcı ID'niz...", description=ctx.author.id, colour="#FFA500",))
 
 bot.run()
