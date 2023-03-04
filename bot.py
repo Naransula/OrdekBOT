abd3dbfd1ef86c421f3525c820fbd296d8e49440
Naransula <121567218+Naransula@users.noreply.github.com>
2023-03-04 10:04:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,28 +20,6 @@ async def on_started(event):
 def rp_mod(context: lightbulb.Context) -> bool:
     return context.author.id == config.rp_mods
 
-@lightbulb.Check
-def owner(context: lightbulb.Context) -> bool:
-    return context.author.id == config.owner_id
-
-@bot.command
-@lightbulb.option("cümle", "Söylenecek cümle.")
-@lightbulb.command("ooc", "OOC Konuşmanızı sağlar!")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def yaz(ctx):
-    proxy = await ctx.respond(f"// {ctx.author.mention}: {ctx.options.cümle}")
-    message = await proxy.message()     
-    await asyncio.sleep(10)
-    await message.delete()
-
-@bot.command
-@lightbulb.add_checks(owner)
-@lightbulb.option("cümle", "Söylenecek cümle.")
-@lightbulb.command("söyle", "Botu konuşturur!")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def yaz(ctx):
-    await ctx.respond(hikari.Embed(title="Naransula diyor ki:", description=ctx.options.cümle, colour="#FFA500"))
-
 @bot.command
 @lightbulb.command("id", "Kullanıcı ID'nizi öğrenin.")
 @lightbulb.implements(lightbulb.SlashCommand)
